32f11ce62863643dcdb476a4f5ecc054e1fe5baa
maxhaesslein <git@maxhaesslein.de>
2023-07-12 14:34:46 +0200

    backwards compatibility for token revocation via token endpoint

--- system/endpoints/token.php
+++ system/endpoints/token.php
@@ -6,7 +6,13 @@ if( ! $core ) exit;
 
 $query = $core->route->get('query');
 
-if( ! empty($query['grant_type']) && $query['grant_type'] == 'refresh_token' ) {
+if( ! empty($query['action']) && $query['action'] == 'revoke' ) {
+	// NOTE: backwards compatibility for token revocation
+
+	include( $core->abspath.'system/endpoints/revocation.php' );
+	exit;
+
+} elseif( ! empty($query['grant_type']) && $query['grant_type'] == 'refresh_token' ) {
 	// refresh token
 
 	include( $core->abspath.'system/endpoints/token/refresh.php' );
