20bdaf3d422c51333ef7c4fae3f9d3d30ac5fb9e
maxhaesslein <git@maxhaesslein.de>
2023-08-02 13:41:47 +0200

    show 'users' config option in homestead-control

--- system/config_definitions.php
+++ system/config_definitions.php
@@ -12,4 +12,8 @@ return [
 		'description' => 'not all themes support (all) color schemes',
 		'options' => ['default' => 'Default (blue)', 'green' => 'Green', 'red' => 'Red', 'lilac' => 'Lilac'],
 	],
+	'users' => [
+		'type' => 'complex',
+		'description' => 'these users are allowed to log in; you can use the <code>generate-password</code> endpoint to get hashed passwords. see <a href="https://github.com/maxhaesslein/einwohnermeldeamt#additional-identities" target="_blank" rel="noopener">Additional Identities</a> in the README for details.'
+	]
 ];
